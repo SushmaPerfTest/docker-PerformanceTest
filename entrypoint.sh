e4849c21e64fbaaa375b5260bb5ae90e264f438f
SushmaSaloni <sushma.saloni@numerator.com>
2022-12-21 00:24:17 +0530

    just like that

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,13 +25,14 @@ export JVM_ARGS="-Xmn${JVM_XMN}m -Xms${JVM_XMS}m -Xmx${JVM_XMX}m"
 
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
-echo "jmeter args=-n -t test/trivial/test-plan.jmx -l test/trivial/test-plan.jtl"
+#echo "jmeter args=-n -t test/trivial/test-plan.jmx -l test/trivial/test-plan.jtl"
+echo "jmeter args=$@"
 
 # Keep entrypoint simple: we must pass the standard JMeter arguments
 #EXTRA_ARGS=-Dlog4j2.formatMsgNoLookups=true
 #echo "jmeter ALL ARGS=${EXTRA_ARGS} $@"
 #jmeter ${EXTRA_ARGS} $@
-jmeter -n -t test/trivial/test-plan.jmx -l test/trivial/test-plan.jtl
+jmeter $@
 
 echo "END Running Jmeter on `date`"
 
